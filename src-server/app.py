f8c3893f15ce95c136fa4b0ae5163b5dd487af18
thais <thaisbarrosalvim@gmail.com>
2024-09-29 15:05:20 -0300

    update to flask on server

--- src-server/app.py
+++ src-server/app.py
@@ -1,7 +1,9 @@
 import requests
 from flask import Flask, render_template, request, jsonify
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
 
 @app.route("/")
 def home():
