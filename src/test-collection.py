3557821a66373a6af1063e63c019131cdc1b7084
thais <thaisbarrosalvim@gmail.com>
2024-10-03 14:40:59 -0300

    update test-collection.py

--- src/test-collection.py
+++ src/test-collection.py
@@ -1,12 +1,16 @@
 import weaviate
 
-weaviate_client = weaviate.connect_to_local()
-collection = weaviate_client.collections.get("ProteinCollection")
-
-count = 0
-for item in collection.iterator():
-    print(item.uuid, item.properties)
-    count += 1
-    if count > 3:
-        break
-weaviate_client.close()
+weaviate_client = weaviate.connect_to_local(host='weaviate')
+try:
+    collection = weaviate_client.collections.get("ProteinCollection")
+    print('\n\nCollection: Collection: Collection: Collection: Collection: ', collection, '\n\n\n\n')
+    print('ITEMS ITEMS ITEMS ITEMS ITEMS ITEMS ITEMS ITEMS ITEMS ITEMS ITEMS ')
+    count = 0
+    collections = weaviate_client.collections.list_all()
+    for item in collection.iterator():
+        print(item.uuid, item.properties)
+        count += 1
+        if count > 3:
+            break
+finally:
+    weaviate_client.close()
