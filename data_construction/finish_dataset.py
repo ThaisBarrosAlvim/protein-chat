6eda7b4a3a4a32d3e2bf3f4c52e59cf23b359696
thais <thaisbarrosalvim@gmail.com>
2024-10-07 10:06:15 -0300

    update fine tuning

--- data_construction/finish_dataset.py
+++ data_construction/finish_dataset.py
@@ -1,36 +1,31 @@
 import json
+import random
 
-def convert_json_to_jsonl(input_json_file, output_jsonl_file):
-    """
-    Converte um arquivo JSON contendo uma lista de objetos em um arquivo JSONL.
 
-    Args:
-        input_json_file (str): O caminho do arquivo JSON de entrada.
-        output_jsonl_file (str): O caminho do arquivo JSONL de saída.
-    """
-    try:
-        # Abrir e carregar o arquivo JSON
-        with open(input_json_file, 'r') as json_file:
-            data = json.load(json_file)
+def shuffle_and_truncate_jsonl(input_file, output_file, limit):
+    data = []
 
-        # Verificar se o dado carregado é uma lista
-        if isinstance(data, list):
-            # Abrir o arquivo JSONL para escrita
-            with open(output_jsonl_file, 'w') as jsonl_file:
-                # Escrever cada objeto da lista em uma linha separada
-                for obj in data:
-                    jsonl_file.write(json.dumps(obj) + '\n')
-            print(f"Conversão para JSONL concluída. Arquivo salvo como {output_jsonl_file}.")
-        else:
-            print("O arquivo JSON não contém uma lista.")
-    except FileNotFoundError:
-        print(f"Arquivo {input_json_file} não encontrado.")
-    except json.JSONDecodeError:
-        print(f"Erro ao decodificar o arquivo JSON {input_json_file}.")
+    # Abrir o arquivo JSONL e carregar os dados
+    with open(input_file, 'r') as infile:
+        for line in infile:
+            data.append(json.loads(line.strip()))
+
+    # Embaralhar os dados
+    random.shuffle(data)
+
+    # Truncar os dados até o limite escolhido
+    truncated_data = data[:limit]
+
+    # Salvar os dados truncados no arquivo de saída
+    with open(output_file, 'w') as outfile:
+        for entry in truncated_data:
+            json.dump(entry, outfile)
+            outfile.write('\n')
+
 
 # Exemplo de uso
-if __name__ == '__main__':
-    input_json_file = 'dataset_protein_articles.json'  # Arquivo JSON de entrada
-    output_jsonl_file = 'dataset_protein_articles.jsonl'  # Arquivo JSONL de saída
+input_file = 'dataset_protein_articles.jsonl'
+output_file = 'dataset_protein_articles_trunc.jsonl'
+limit = 1500  # Número de linhas truncadas
 
-    convert_json_to_jsonl(input_json_file, output_jsonl_file)
+shuffle_and_truncate_jsonl(input_file, output_file, limit)
